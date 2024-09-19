fb983bf7b41d6e98f9cf275c045350b21e9327d9
Tomáš Linhart <pasmen@gmail.com>
2024-09-19 08:40:56 +0200

    Handle pagination when listing repositories

--- compare-local-remote.sh
+++ compare-local-remote.sh
@@ -6,7 +6,18 @@ BASE_DIR=${BASE_DIR:-$(pwd)}
 BASE_DIR=$(sed "s|//*|/|g; s|/*$||" <<< $BASE_DIR)
 
 local=$(find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d ! -name "*.wiki" -exec basename {} \; | sort)
-remote=$(curl -s -H "Authorization: token ${TOKEN}" "https://api.github.com/orgs/$ORG/repos?per_page=100" | jq -r '.[].name' | sort)
+remote=""
+page=1
+while true; do
+  repos=$(curl -s -H "Authorization: token ${TOKEN}" "https://api.github.com/orgs/$ORG/repos?per_page=100&page=${page}" | jq -r '.[].name')
+  if [ -n "${repos}" ]; then
+    remote="${remote}"$'\n'"${repos}"
+    page=$((page + 1))
+  else
+    break
+  fi
+done
+remote=$(echo "$remote" | sort)
 echo "Missing locally"
 comm -13 <(echo "$local") <(echo "$remote") | sed "s/^/    /"
 echo "Missing in remote"
